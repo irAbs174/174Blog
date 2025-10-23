9f9792a96a606f16d49d49cec0dcf01e73ed7d3d
48845 <108906651+irAbs174@users.noreply.github.com>
2025-10-24 02:21:15 +0330

    update api/blog => add more data

--- api/blog.py
+++ api/blog.py
@@ -5,11 +5,20 @@ from django.views.decorators.csrf import csrf_exempt
 def post_list(request):
     return JsonResponse({
         'success': True,
-        'data': {
-            'title':'هک اخلاقی',
-            'image':'/static/images/pngs/4.jpg',
-            'category': 'علمی تخیلی',
-            'writer':'عباس دمرچی',
-            'url':'/'
+        'data': [
+            {
+                'title':'هک اخلاقی',
+                'image':'/static/images/pngs/4.jpg',
+                'category': 'علمی تخیلی',
+                'writer':'عباس دمرچی',
+                'url':'/'
+                },
+            {
+                'title':'دوره سلامتی',
+                'image':'/static/images/pngs/5.jpg',
+                'category': 'علمی جنایی',
+                'writer':'عباس دمرچی',
+                'url':'/'
             }
+        ]
     })
