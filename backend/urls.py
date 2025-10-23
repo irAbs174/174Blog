089afad1c416275d7f9d35ae824b70181e06144e
48845 <108906651+irAbs174@users.noreply.github.com>
2025-10-24 01:56:10 +0330

    update backend/urls.py => add api urls path

--- backend/urls.py
+++ backend/urls.py
@@ -2,12 +2,13 @@
 URL configuration for 174Blog project.
 """
 from django.conf.urls.static import static
+from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path
 from .views import index
 
 urlpatterns = [
+    path('api/', include("api.urls") ),
     path('admin/', admin.site.urls),
     path('', index),
 ]
