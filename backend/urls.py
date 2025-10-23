5b718d09053ac678cffb52bf03463847f0255324
48845 <108906651+irAbs174@users.noreply.github.com>
2025-10-23 23:57:06 +0330

    update backend/urls.py => add static urls for Serve static files in development

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,17 @@
 """
 URL configuration for 174Blog project.
 """
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
