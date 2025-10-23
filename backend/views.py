f75c9e1f23cc37719ee9c8541cafda38f609ca52
48845 <108906651+irAbs174@users.noreply.github.com>
2025-10-23 23:57:48 +0330

    update backend/views.py => add some data to content dic

--- backend/views.py
+++ backend/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 def index(request):
     content = {
         'title':'174 Blog',
-        'meta': 'simple blog with django and directus'
+        'description': 'simple blog with django and directus',
+        'keywords':'linux and security',
+        'team': '174 Team',
     }
     return render(request, 'index.html', content)
