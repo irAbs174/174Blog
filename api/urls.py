53633cff6cff1a64a61e891d6f29dae6afccd78f
48845 <108906651+irAbs174@users.noreply.github.com>
2025-10-24 22:13:38 +0330

    index page camplated

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from .blog import post_list
-from .menu import menu
 
 urlpatterns = [
-    path('menu/list', menu),
     path('blog/post/list', post_list),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    res = requests.get('https://strange-kare-ive7vog0s.liara.run/items/menu').json()['data']
+    id_map = {item['id']: {**item, 'children': []} for item in res}
+    menu = []
+    for item in id_map.values():
+        if item['parent']:
+            parent = id_map.get(item['parent'])
+            if parent:
+                parent['children'].append(item)
+        else:
+            menu.append(item)
     content = {
         'title':'174 Blog',
         'description': 'simple blog with django and directus',
         'keywords':'linux and security',
         'team': '174 Team',
+        'menu': menu,
+        'logo': 'https://strange-kare-ive7vog0s.liara.run/assets/394e1968-f966-4df3-b462-c6581b423380.png',
     }
     return render(request, 'index.html', content)
