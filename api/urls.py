468f462923f4fbcce31be0c0a699631f9febd049
48845 <108906651+irAbs174@users.noreply.github.com>
2025-10-24 21:41:26 +0330

    update frontend => add dynamic menu

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .blog import post_list
+from .menu import menu
 
 urlpatterns = [
-    path('blog/post/list', post_list)
+    path('menu/list', menu),
+    path('blog/post/list', post_list),
 ]
